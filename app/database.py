225c5fda7ef678512c155f8dfb1940089ae8af02
SergioGuarin117 <sergioguarin117@gmail.com>
2026-04-09 15:09:58 -0500

    Fix: Problemas con los test y la base de datos

--- app/database.py
+++ app/database.py
@@ -10,7 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./fallback.db")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False} if DATABASE_URL.startwith("sqlite") else {})
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
