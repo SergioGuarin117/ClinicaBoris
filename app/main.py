45d2167ddd0cd9eaa5ededc1fe4b09d127f052e5
SergioGuarin117 <sergioguarin117@gmail.com>
2026-05-23 15:21:10 -0500

    Add production URL to CORS

--- app/main.py
+++ app/main.py
@@ -28,7 +28,10 @@ app = FastAPI(
  
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:3000",
+    "https://TU-APP.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
